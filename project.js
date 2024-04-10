d4617ff675c93435c8df530553eb69ff9ef05d28
Mike De Leon <mdeleon@horizonmedia.com>
2024-04-10 16:36:25 -0400

    trying to add Shohei pitching stats

--- project.js
+++ project.js
@@ -11,19 +11,36 @@ playerSelect.addEventListener("change",()=>{
 const players = [
   {
                     
-                    name: "Juan Soto",
-                    year: 2020,
-                    slash: ".351/.490/.695",
-                    OWAR: 3.2,
-                    adjustedOPS: "217"
+            name: "Juan Soto",
+            year: 2020,
+            slash: ".351/.490/.695",
+            OWAR: 3.2,
+            adjustedOPS: "217"
   },
   {
-                    name: "Ronald Acuña",
-                    year: 2023,
-                    slash: ".337/.416/.596",
-                    OWAR: 8.5,
-                    adjustedOPS: "168"
-  }
+            name: "Ronald Acuña",
+            year: 2023,
+            slash: ".337/.416/.596",
+            OWAR: 8.5,
+            adjustedOPS: "168"
+  },
+  {
+            name: "Aaron Judge",
+            year: 2022,
+            slash: ".311/.425/.686",
+            OWAR: 10.3,
+            adjustedOPS: "210"
+  },
+  {
+            name: "Shohei Ohtani",
+            year: 2021,
+            slash: ".311/.425/.686",
+            OWAR: 10.3,
+            adjustedOPS: "210",
+            ERA: 3.14,
+            IP: 132,
+            K: 168
+}
 ];
 
 function getPlayerStats(playerName){
@@ -35,16 +52,26 @@ function getPlayerStats(playerName){
           return;
         }
       }
+      
   }
   function displayPlayerStats(player) {
   
-  const statsHTML = `
+  let statsHTML = `
     <h3>${player.name}</h3>
     <p>Year: ${player.year}</p>
     <p>Slash Line: ${player.slash}</p>
     <p>OWAR: ${player.OWAR}</p>
     <p>Adjusted OPS: ${player.adjustedOPS}</p>
   `;
+
+  if (selectedPlayer.hasOwnProperty('ERA')) {
+        statsHTML += `
+          <p>ERA: ${player.ERA}</p>
+          <p>IP: ${player.IP}</p>
+          <p>K: ${player.K}</p>
+        `;
+      }
+  
     playerStatsDiv.innerHTML = statsHTML;
     playerStatsDiv.classList.remove('hidden');
     return;
