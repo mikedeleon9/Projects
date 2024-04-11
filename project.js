6c5c6ee7c0633f89003edc60904bc77ec66389b7
Mike De Leon <mdeleon@horizonmedia.com>
2024-04-11 13:32:15 -0400

    added bios and other changes

--- project.js
+++ project.js
@@ -1,5 +1,6 @@
 const playerStatsDiv = document.getElementById("player-stats");
   const playerSelect = document.getElementById("player-select");
+  const playerBioDiv = document.querySelector(".player-bio");
 
 playerSelect.addEventListener("change",()=>{
 
@@ -15,36 +16,27 @@ const players = [
             year: 2020,
             slash: ".351/.490/.695",
             OWAR: 3.2,
-<<<<<<< HEAD
             adjustedOPS: "217",
-            image: "PlayerImages/Soto.jpg"
-=======
-            adjustedOPS: "217"
->>>>>>> 569b8dc8363d093d3f2d8d6ef574d06433931831
+            image: "PlayerImages/Soto.jpg",
+            bio: "Juan Soto is a young superstar outfielder for the New York Yankees. Known for his elite pitch recognition and plate discipline, Soto has emerged as one of the most feared hitters in baseball."
   },
   {
             name: "Ronald Acuña",
             year: 2023,
             slash: ".337/.416/.596",
             OWAR: 8.5,
-<<<<<<< HEAD
             adjustedOPS: "168",
-            image: "PlayerImages/Acuna.jpg"
-=======
-            adjustedOPS: "168"
->>>>>>> 569b8dc8363d093d3f2d8d6ef574d06433931831
+            image: "PlayerImages/Acuna.jpg",
+            bio: "Ronald Acuña is a dynamic outfielder known for his electrifying speed, powerful hitting, and strong defensive skills. He has quickly become one of the most exciting players in baseball, showcasing an impressive combination of athleticism and talent."
   },
   {
             name: "Aaron Judge",
             year: 2022,
             slash: ".311/.425/.686",
             OWAR: 10.3,
-<<<<<<< HEAD
             adjustedOPS: "210",
-            image: "PlayerImages/Judge.jpg"
-=======
-            adjustedOPS: "210"
->>>>>>> 569b8dc8363d093d3f2d8d6ef574d06433931831
+            image: "PlayerImages/Judge.jpg",
+            bio: "Aaron Judge is a towering presence in the outfield and a prolific slugger at the plate. Known for his mammoth home runs and strong arm, Judge is a fan favorite for the New York Yankees and a dominant force in the game."
   },
   {
             name: "Shohei Ohtani",
@@ -52,13 +44,11 @@ const players = [
             slash: ".311/.425/.686",
             OWAR: 10.3,
             adjustedOPS: "210",
-<<<<<<< HEAD
             image: "PlayerImages/Ohtani.jpeg",
-=======
->>>>>>> 569b8dc8363d093d3f2d8d6ef574d06433931831
             ERA: 3.14,
             IP: 132,
-            K: 168
+            K: 168,
+            bio:"Shohei Ohtani is a rare two-way player who excels both as a pitcher and a hitter. He captivates audiences with his blazing fastballs, devastating breaking pitches, and explosive home runs. Ohtani's versatility makes him a standout talent in modern baseball."
 },
 {
             name: "Bryce Harper",
@@ -66,10 +56,8 @@ const players = [
             slash: ".309/.429/.615",
             OWAR: 6.5,
             adjustedOPS: "179",
-<<<<<<< HEAD
-            image: "PlayerImages/Harper.jpg"
-=======
->>>>>>> 569b8dc8363d093d3f2d8d6ef574d06433931831
+            image: "PlayerImages/Harper.jpg",
+            bio: "Bryce Harper is an intense and passionate outfielder recognized for his power hitting and aggressive play style. He possesses a strong arm, exceptional bat speed, and a flair for the dramatic moments on the field."
 },
 {
             name: "Freddie Freeman",
@@ -77,10 +65,8 @@ const players = [
             slash: ".331/.410/.567",
             OWAR: 7.5,
             adjustedOPS: "161",
-<<<<<<< HEAD
-            image: "PlayerImages/Freeman.jpg"
-=======
->>>>>>> 569b8dc8363d093d3f2d8d6ef574d06433931831
+            image: "PlayerImages/Freeman.jpg",
+            bio: "Freddie Freeman is a consistent and dependable first baseman known for his smooth swing, exceptional fielding, and leadership qualities. He's been a cornerstone player for the Atlanta Braves, contributing both offensively and defensively."
 },
 {
             name: "Corey Seager",
@@ -88,10 +74,8 @@ const players = [
             slash: ".327/.390/.623",
             OWAR: 6.4,
             adjustedOPS: "169",
-<<<<<<< HEAD
-            image: "PlayerImages/Seager.jpg"
-=======
->>>>>>> 569b8dc8363d093d3f2d8d6ef574d06433931831
+            image: "PlayerImages/Seager.jpg",
+            bio: "Corey Seager is a skilled shortstop celebrated for his smooth fielding and clutch hitting. He's known for his ability to drive in runs and make spectacular defensive plays, earning him accolades as one of the top players at his position."
 },
 {
             name: "Vladimir Guerrero Jr",
@@ -99,10 +83,8 @@ const players = [
             slash: ".311/.401/.601",
             OWAR: 6.4,
             adjustedOPS: "167",
-<<<<<<< HEAD
-            image: "PlayerImages/Vlad.jpg"
-=======
->>>>>>> 569b8dc8363d093d3f2d8d6ef574d06433931831
+            image: "PlayerImages/Vlad.jpg",
+            bio: "Vladimir Guerrero Jr is a young powerhouse hitter with exceptional bat-to-ball skills and tremendous raw power. Following in his father's footsteps, he's quickly making a name for himself as one of the most promising hitters in the league."
 },
 {
             name: "Matt Olson",
@@ -110,10 +92,8 @@ const players = [
             slash: ".283/.389/.604",
             OWAR: 6.6,
             adjustedOPS: "162",
-<<<<<<< HEAD
-            image: "PlayerImages/Olson.jpeg"
-=======
->>>>>>> 569b8dc8363d093d3f2d8d6ef574d06433931831
+            image: "PlayerImages/Olson.jpeg",
+            bio: "Matt Olson is a standout first baseman known for his superb defensive abilities and powerful left-handed swing. He's emerged as a key player for the Oakland Athletics, providing offensive production and stellar defense."
 },
 {
             name: "Fernando Tatis Jr",
@@ -121,10 +101,8 @@ const players = [
             slash: ".282/.364/.611",
             OWAR: 7.3,
             adjustedOPS: "166",
-<<<<<<< HEAD
-            image: "PlayerImages/Tatis.jpg"
-=======
->>>>>>> 569b8dc8363d093d3f2d8d6ef574d06433931831
+            image: "PlayerImages/Tatis.jpg",
+            bio: "Fernando Tatis Jr is an electrifying shortstop with an exciting blend of speed, power, and defensive prowess. His flair for the dramatic and highlight-reel plays has made him a rising star in Major League Baseball."
 }
 ];
 
@@ -142,16 +120,19 @@ function getPlayerStats(playerName){
   function displayPlayerStats(player) {
   
   let statsHTML = `
-<<<<<<< HEAD
     <img src="${player.image}" alt="${player.name}'s Image" class="player-image">
-=======
->>>>>>> 569b8dc8363d093d3f2d8d6ef574d06433931831
     <h3>${player.name}</h3>
     <p>Year: ${player.year}</p>
     <p>Slash Line: ${player.slash}</p>
     <p>OWAR: ${player.OWAR}</p>
     <p>OPS+: ${player.adjustedOPS}</p>
   `;
+
+  let bioHTML =`
+    <h3>Player Bio</h3>
+    <p>${player.bio}</p>
+  `;
+  
 
   if (player.name === 'Shohei Ohtani') {
         statsHTML += `
@@ -165,6 +146,12 @@ function getPlayerStats(playerName){
       }
   
     playerStatsDiv.innerHTML = statsHTML;
-    playerStatsDiv.classList.remove('hidden');
+
+    document.querySelector('.player-bio').innerHTML = bioHTML;
+    playerBioDiv.innerHTML = bioHTML;
+    playerStatsDiv.style.display = "block";
+
+    playerBioDiv.style.display = "block";
+    
     return;
   }
